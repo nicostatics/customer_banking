9531d3ce9fa3cbd815964a8ec15ae14c58c64ecf
nicostatics <44653707+nicostatics@users.noreply.github.com>
2024-04-04 12:54:46 -0700

    fixed banking variable name to savings_banking

--- savings_account.py
+++ savings_account.py
@@ -18,7 +18,7 @@ def create_savings_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
-    banking = Account(balance, interest_rate)
+    savings_banking = Account(balance, interest_rate)
 
     # Calculate interest earned
     # ADD YOUR CODE HERE
@@ -30,11 +30,11 @@ def create_savings_account(balance, interest_rate, months):
 
     # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
-    banking.set_balance(balance)
+    savings_banking.set_balance(balance)
 
     # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
-    banking.set_interest(interest_earned)
+    savings_banking.set_interest(interest_earned)
 
     # Return the updated balance and interest earned.
     # ADD YOUR CODE HERE
